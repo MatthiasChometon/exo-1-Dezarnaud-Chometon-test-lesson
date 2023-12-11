198f3cdb58d03d8a1305153bcc191e2833f633ff
Johanna Dézarnaud <johanna.dezarnaud@ynov.com>
2023-12-11 10:11:15 +0100

    feat: first test

---
+++ mirror.test.ts
@@ -0,0 +1,5 @@
+import { mirrorTest } from "./mirror2";
+
+it("reverse word", () => {
+  expect(mirrorTest("HELLO")).toBe("OLLEH");
+});
---
+++ mirror2.ts
@@ -0,0 +1,3 @@
+export function mirrorTest(str: string): string {
+  return str.split("").reverse().join("");
+}
